b53e659ce3ab744f71e0de46db7dfd03d83eae8c
anmers3 <anmers3@gmail.com>
2021-12-22 19:13:25 -0300

    first version

---
+++ main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from twilio.rest import Client
+import calendar
+from python_enviar_sms.sms_auth_token import *
+
+account_sid = account_sid()
+auth_token = auth_token()
+client = Client(account_sid, auth_token)
+
+month_list = []
+for month_idx in range(1, 7):
+    # monthList.append(calendar.month_name[month_idx])
+    month_list.append(calendar.month_abbr[month_idx])
+
+def find_doc (month_nickname):
+    sales_table = pd.read_excel(f'/home/anmer.machado/IdeaProjects/python-programs/python_enviar_sms/arquivos/{month_nickname}.xlsx')
+    return sales_table
+
+for month in month_list:
+    # print(month)
+    monthlyData = find_doc(month)
+    # print(monthlyData)
+
+    if (monthlyData['Vendas'] >= 55000).any():
+        vendedor = monthlyData.loc[monthlyData['Vendas'] >= 55000, 'Vendedor'].values[0]
+        vendas = monthlyData.loc[monthlyData['Vendas'] >= 55000, 'Vendas'].values[0]
+        print(f'No mês {month} , {vendedor} bateu as metas com R$: {vendas}')
+        message = client.messages.create(
+            to="+5548999209186",
+            from_="+12543454332",
+            body=f'No mês {month} , {vendedor} bateu as metas com R$: {vendas}')
+        print(message.sid)
---
+++ sms_auth_token.py
@@ -0,0 +1,7 @@
+def account_sid():
+    account_sid = "ACdbf79abbd6c56c70a0939794d7adedf8"     # Your Account SID from twilio.com/console
+    return account_sid
+
+def auth_token():
+    auth_token = "9876765b42e7f4df503518dd386be03e"        # Your Auth Token from twilio.com/console
+    return auth_token
